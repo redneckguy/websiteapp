8b35766635a5a59ece04bdfd0886927e3346e5cf
Dmitry <dmitryvo83@gmail.com>
2020-04-12 22:50:44 +0300

    website application task7
    version1

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let btnStartExpens = document.getElementById('start');
+let valueBlocks = document.querySelectorAll('[class*="-value"]');
+let expensesItems = document.querySelectorAll('.expenses-item');
+let setOfButtons = document.getElementsByTagName('button');
+let firstExpensesBtn = setOfButtons[0];
+let secondExpensesBtn = setOfButtons[1];
+let budgetButton = setOfButtons[2];
+let optionalExpenses = document.querySelectorAll('.optionalexpenses-item');
+let incomeField = document.querySelector('#income');
+let savingsCheckBox = document.querySelector('#savings');
+let sumField = document.querySelector('#sum');
+let percentField = document.querySelector('#percent');
+let yearField = document.querySelector('.year-value');
+let monthField = document.querySelector('.month-value');
+let dayField = document.querySelector('.day-value');
+
+
+
+
+
+
+
+
